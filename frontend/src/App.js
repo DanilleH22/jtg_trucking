a989913a6981b6cb618049350e1d81e7d8faf99b
Danille Hamilton <danilleham@hotmail.co.uk>
2025-01-03 11:33:55 +0000

    Update function to right router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import ContactUs from "./pages/contact/ContactUs";
+import Services from "./pages/services/Services";
+import AboutUs from "./pages/about/AboutUs";
+import Homepage from "./pages/homepage/Homepage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/contact_us" element={<ContactUs />} />
+        <Route path="*" element={<h1>Page not found!</h1>} />
+      </Routes>
+    </Router>
   );
 }
 
