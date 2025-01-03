a489bd76abbfa9ffe0075b8274cc045a72f9bd1e
Danille Hamilton <danilleham@hotmail.co.uk>
2025-01-03 12:14:53 +0000

    Remove backend project and app and add to backend folder

--- contact/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import ContactUs
-
-urlpatterns = [
-    path('contact/', ContactUs.as_view(), name='contact')
-]
--- quote/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import RequestQuote
-
-urlpatterns = [
-    path('quote/', RequestQuote.as_view())
-]
