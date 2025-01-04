9cdaead035e55531beefcac238ead5633e211fd7
Danille Hamilton <danilleham@hotmail.co.uk>
2025-01-04 18:08:30 +0000

    Update datefield in models for quote app

--- backend/quote/models.py
+++ backend/quote/models.py
@@ -9,7 +9,7 @@ class Quote(models.Model):
     phone_number = models.CharField(max_length=15, blank=False)
     pickup_location = models.CharField(max_length=200, blank=False)
     drop_off_location = models.CharField(max_length=200, blank=False)
-    preferred_date = models.CharField(max_length=15, blank=False)
+    preferred_date = models.DateField(blank=False)
     type_of_freight = models.CharField(max_length=200, blank=False)
     weight_of_freight = models.CharField(max_length=20, blank=True)
     additional_services = models.TextField(max_length=500, blank=True)
