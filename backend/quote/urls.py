115d684ed99807094d1f176c353dd6ed0f252f1e
Danille Hamilton <danilleham@hotmail.co.uk>
2025-01-05 01:07:34 +0000

    Add nme for urls.quote

--- backend/quote/urls.py
+++ backend/quote/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import RequestQuote
 
 urlpatterns = [
-    path('quote/', RequestQuote.as_view())
+    path('quote/', RequestQuote.as_view(), name='request_quote')
 ]
