2a9c024c3e65d06aa6eb3b02b4ee260522412731
Danille Hamilton <danilleham@hotmail.co.uk>
2025-01-09 17:01:16 +0000

    Update axios to push to backend

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,8 +1,16 @@
 import axios from "axios";
 
-axios.defaults.baseURL = [ "http://127.0.0.1:8000/", "https://8000-danilleh22-jtgtrucking-wa22733plo0.ws.codeinstitute-ide.net/"]
-axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
-axios.defaults.withCredentials = true;
+const csrfToken = document.cookie
+  .split("; ")
+  .find((row) => row.startsWith("csrftoken="))
+  ?.split("=")[1];
 
-export const axiosReq = axios.create();
-export const axiosRes = axios.create();
+const instance = axios.create({
+  baseURL: "https://8000-danilleh22-jtgtrucking-wa22733plo0.ws.codeinstitute-ide.net/",
+  headers: {
+    "X-CSRFToken": csrfToken,
+    "Content-Type": "application/json",
+  },
+});
+
+export default instance;
