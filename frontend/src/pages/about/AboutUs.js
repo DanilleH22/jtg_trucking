bcd92a8954ac071f1a624bd06588209524ea52db
Danille Hamilton <danilleham@hotmail.co.uk>
2025-01-22 20:00:53 +0000

    style for the margins

--- frontend/src/pages/about/AboutUs.js
+++ frontend/src/pages/about/AboutUs.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import { Container, Row, Col, Image, Card } from "react-bootstrap";
-import styles from "../../styles/Homepage.module.css";
 import example1 from "../../assets/example1.jpg";
 import example2 from "../../assets/example2.jpg";
 
 const AboutUs = () => {
   return (
     <Container>
-        <Row>
+        <Row className='my-2'>
             <Col>
             <h3 className="d-flex justify-content-center">About Us</h3>
             </Col>
         </Row>
-        <Row>
+        <Row className='my-2'>
             <Col className='d-flex align-items-center justify-content-center'>
                 <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
             </Col>
@@ -20,7 +19,7 @@ const AboutUs = () => {
             <Image src={example2} thumbnail />
             </Col>
         </Row>
-        <Row>
+        <Row className='my-3'>
             <Col>
             <Image src={example1} thumbnail />
             </Col>
@@ -28,7 +27,7 @@ const AboutUs = () => {
             <p >"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
             </Col>
         </Row>
-        <Row className='d-flex justify-content-evenly'>
+        <Row className='d-flex justify-content-evenly my-3'>
           <h4 className="text-center">Testimonials</h4>
         <Card style={{ width: '18rem' }} >
       <Card.Body className="text-center">
