6f32b404e281cad883fdaa850151cf5b65b089fc
Danille Hamilton <danilleham@hotmail.co.uk>
2025-01-22 09:58:25 +0000

    Un comment out post for contact form

--- backend/contact/views.py
+++ backend/contact/views.py
@@ -18,12 +18,12 @@ class ContactUs(APIView):
     # def get(self, request, *args, **kwargs):
     #     return Response({"message": "Contact endpoint is ready for POST requests."}, status=status.HTTP_200_OK)
 
-    # def post(self, request, *args, **kwargs):
-    #     serializer = ContactSerializer(data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, *args, **kwargs):
+        serializer = ContactSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     # def options(self, request, *args, **kwargs):
     #     """
