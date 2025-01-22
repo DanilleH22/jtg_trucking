0f541b062a15e332f82621196011b046aca20121
Danille Hamilton <danilleham@hotmail.co.uk>
2025-01-22 19:42:03 +0000

    Import about us photos

--- frontend/src/pages/about/AboutUs.js
+++ frontend/src/pages/about/AboutUs.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Container, Row, Col, Image, Card } from "react-bootstrap";
 import styles from "../../styles/Homepage.module.css";
+import example1 from "../../assets/example1.jpg";
+import example2 from "../../assets/example2.jpg";
 
 const AboutUs = () => {
   return (
@@ -15,12 +17,12 @@ const AboutUs = () => {
                 <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
             </Col>
             <Col>
-            <Image src="holder.js/171x180" thumbnail />
+            <Image src={example2} thumbnail />
             </Col>
         </Row>
         <Row>
             <Col>
-            <Image src="holder.js/171x180" thumbnail />
+            <Image src={example1} thumbnail />
             </Col>
             <Col>
             <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
