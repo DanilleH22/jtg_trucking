0a496ccae5cc50d61f1a12a8b10bdf7676fa053b
Danille Hamilton <danilleham@hotmail.co.uk>
2025-01-22 19:49:15 +0000

    Centre about use text

--- frontend/src/pages/about/AboutUs.js
+++ frontend/src/pages/about/AboutUs.js
@@ -13,7 +13,7 @@ const AboutUs = () => {
             </Col>
         </Row>
         <Row>
-            <Col>
+            <Col className='d-flex align-items-center justify-content-center'>
                 <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
             </Col>
             <Col>
@@ -24,8 +24,8 @@ const AboutUs = () => {
             <Col>
             <Image src={example1} thumbnail />
             </Col>
-            <Col>
-            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            <Col className='d-flex align-items-center justify-content-center'>
+            <p >"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
             </Col>
         </Row>
         <Row className='d-flex justify-content-evenly'>
